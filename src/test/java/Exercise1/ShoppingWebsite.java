a445201eff490b63e61f98f8f63933d9f9ffd8d3
JWahed <jak.clev@gmail.com>
2022-08-15 17:11:31 +0100

    Update ShoppingWebsite.java

--- src/test/java/Exercise1/ShoppingWebsite.java
+++ src/test/java/Exercise1/ShoppingWebsite.java
@@ -3,21 +3,20 @@ package Exercise1;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import org.openqa.selenium.By;
-
 import org.openqa.selenium.Keys;
-
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.RemoteWebDriver;
-
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class ShoppingWebsite {
 
